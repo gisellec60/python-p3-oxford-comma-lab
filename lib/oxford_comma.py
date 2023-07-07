e10c57ba082f6d316a1b952ec49868a6e6080182
Giselle <gisellec60@gmail.com>
2023-07-07 17:34:00 -0400

    test passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1 :
+       return "".join(items)  
+    elif len(items) == 2:
+       return " and ".join(items)
+    else:
+        str_item=""
+        i=1
+        for item in items:
+            if (len(items) == i ):
+              str_item=str_item + "and " + item
+            else:  
+              i+=1
+              str_item=str_item + item + ", "
+    return str_item
+
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]))
